a06ba908010775395ed24002bfb4ac65ebe89f22
MoralesDiaz <omoralesd2100@alumno.ipn.mx>
2022-09-07 17:02:51 -0500

    Ejemplo de entrada de texto

---
+++ 02EntradaDeDatos/EntradaTexto.java
@@ -0,0 +1,33 @@
+//esto es un comentario 
+
+/*
+ * todo programa en java debe tener el nombre de la clase tal cual como el archivo 
+ *
+ */
+
+class Entradatexto {
+   // los limites de la clase
+
+   // toda clase debe de tener un metodo principal
+   /**
+    * vamos a realizar un programada mediante el cual podamos introducir texto a
+    * nuestra conveniencia
+    */
+
+   // tipo de dato
+
+   // el; es el fin de linea
+
+   // metodo principal
+   public static void main(String[] args) {
+      String nombre;
+      // instancia
+      System.out.println("por favor introduce tu nombre bien kawai");
+
+      // vamos a obtener el nombre del usuario
+
+      nombre = System.console().readLine();
+
+      System.out.println("Hola " + nombre + "bienvenido a tu programita");
+   }
+}
